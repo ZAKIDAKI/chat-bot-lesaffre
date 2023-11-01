b4e452a609a0be1362d01e63ec2f50cf467a37b4
Zaki <zakaria.mouchtati01@gmail.com>
2023-11-01 09:13:15 +0100

    HELLO

--- index.js
+++ index.js
@@ -5,29 +5,6 @@ const { sendMessage } =require("./utlis/send-message")
 const { welcomeMessage } = require("./utlis/default")
 require('dotenv').config()
 const fs = require('fs');
-const { PrismaClient } = require("@prisma/client")
-
-const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: "mysql://pro:}r>7KYPWOV+EVOZK@localhost:3306/chat_bot_BR_BREAD",
-      },
-    },
-  })
-
-
-
-app.get("/",async (req,res)=>{
-    const response=await prisma.lead.create({
-        data:{
-            phone:"212621586010",
-            lang:"FR",
-            profileName:"ZAKARIA MOUCHTATI"
-        }
-    })
-
-    res.send("CHAT BOT LESAFFRE").status(200)
-})
 
 
 app.get("/leads",async (req,res)=>{
@@ -37,7 +14,8 @@ app.get("/leads",async (req,res)=>{
 })
 
 app.post("/chat-bot",(req,res)=>{
-
+    console.log(req.body)
+    
     const message = req.body
 
     const option= {
@@ -74,34 +52,3 @@ function logToFile(message) {
       }
     });
 }
-
-
-
-
-// const express=require("express")
-// const app=express()
-// require('dotenv').config()
-// var mysql      = require('mysql');
-
-// const connection = module.exports =  mysql.createConnection({
-//   host     : process.env.HOST,
-//   user     : process.env.DATABASE_USER,
-//   password : process.env.DATABASE_PASSWORD,
-//   database : process.env.DATABASE
-// });
- 
-
-// connection.connect((err) => {
-//     if (err) {
-//       console.error('Error connecting to MySQL database: ' + err.stack);
-//       return;
-//     }
-//     console.log('Connected to MySQL database as ID ');
-// });
-
-
-// app.get("/",async (req,res)=>{
-//     res.send("CHAT BOT LESAFFREcasad").status(200)
-// })
-
-// app.listen(5000,()=>console.log("App Started !"))
