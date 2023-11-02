77c1120c7de8c1becfd23e00611a9d70326e4b9d
Zaki <zakaria.mouchtati01@gmail.com>
2023-11-02 16:56:09 +0100

    ADD RANTING

--- utlis/messages.js
+++ utlis/messages.js
@@ -1,7 +1,7 @@
 const connection = require('../database/connect');
 
 exports.getLastMessage=(phone,callback)=>{
-    let req="select *from message where createdAt=? order by created_at limit 1"
+    let req="select *from message where from=? order by createdAt limit 1"
 
     connection.query(req,[phone],(error,results)=> {
         if(error) console.log(error)
